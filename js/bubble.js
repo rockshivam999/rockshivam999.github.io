08b3742d8fcbc4e3b5b56b1101875033742e86b6
Prem <rockshivam999@gmail.com>
2021-04-28 13:38:33 +0530

    added bubble sort visulizer

--- js/bubble.js
+++ js/bubble.js
@@ -1,28 +1,36 @@
-const board=document.getElementById("board");
-const ctx=board.getContext("2d");
-board.width=1000;
-board.height=1000;
+const board = document.getElementById("board1");
+const ctxofbubble = board.getContext("2d");
+// board.width = window.innerWidth - 50;
+// board.height = window.innerHeight - 50;
 
 // ctx.moveTo(0,0);
 // ctx.lineTo(200,200);
 // ctx.stroke();
-ctx.strokeStyle = "#FF0000";
+// ctxofbubble;
+// ctx.strokeWidth=1;
+// ctx.lineWidth = 0;
+var arr = [];
 
-var arr=[10,15,20,18,44,20,45,99,34,44,324,65,24,65,23];
+function generateRandomValued() {
+    for (var i = 0; i < 80; i++) {
+        arr.push((5 + Math.floor(Math.random() * 100))%board.height)
+    }
+}
 
-function draw(arr){
-    ctx.clearRect(0,0,1000,1000);
+function draw(ctx ,arr) {
+    ctx.strokeStyle = "#ff7a5a"
+    ctx.clearRect(0,0,board.width,board.height);
     ctx.beginPath();
-    ctx.moveTo(0,0);
+    ctx.moveTo(0, 0);
     ctx.stroke();
-    var y=500;
-    var move=40;
-    var x=50;
+    var y = board.height;
+    var move =board.width / arr.length;
+    var x = 0;
     // ctx.fillStyle = "red";
     arr.forEach(num => {
-        ctx.moveTo(x,y);
-        ctx.rect(x,y,move,-num);
-        x+=move
+        ctx.moveTo(x, y);
+        ctx.rect(x, y, move, -num);
+        x += move
         ctx.stroke()
     });
     // console.log("hello")
@@ -31,62 +39,92 @@ function draw(arr){
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
-  function delaycallback(delaytime,arrt){
+function delaycallback(delaytime, arrt,ctxofbubble) {
     sleep(delaytime)
-    .then(() => { draw(arrt)});
-  }
+        .then(() => { draw(ctxofbubble,arrt) });
+}
 
-function bubblesort(arr){
-    var k=2;
-    for(var i=0;i<arr.length;i++){
-        for(var j=1;j<arr.length-i;j++){
-                if(arr[j-1]>arr[j]){
-                    var t=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=t;
-                    
-                }
-                k++
-                var arrt=arr.slice(0); 
-                delaycallback(k*100,arrt);
-               
-                
+function bubblesort(arr,ctxofbubble) {
+    var k = 2;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 1; j < arr.length - i; j++) {
+            if (arr[j - 1] > arr[j]) {
+                var t = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = t;
+
+            }
+            k++
+            var arrt = arr.slice(0);
+            delaycallback(k * 10, arrt,ctxofbubble);
+
+
         }
     }
 }
 
 // bubblesort(arr);
-var k=1;
-function quicksort(arr,l,r){
-    console.log(k+" "+l+" "+r);
-    if(r<=l){
+var k = 1;
+function quicksort(arr, l, r) {
+    console.log(k + " " + l + " " + r);
+    if (r <= l) {
         return;
-    }else{
-        var p=l+Math.floor(Math.random() * 100)%(r-l+1);
-        var curr=l;
-        var t=arr[p];
-        arr[p]=arr[r];
-        arr[r]=t;
+    } else {
+        var p = l + Math.floor(Math.random() * 100) % (r - l + 1);
+        var curr = l;
+        var t = arr[p];
+        arr[p] = arr[r];
+        arr[r] = t;
 
-        for(var i=l;i<r;i++){
-            if(arr[i]<=t){
-                var x=arr[i];
-                arr[i]=arr[curr];
-                arr[curr]=x;
-                cur++;
+        for (var i = l; i < r; i++) {
+            if (arr[i] <= t) {
+                var x = arr[i];
+                arr[i] = arr[curr];
+                arr[curr] = x;
+                curr++;
             }
             k++;
-            var arrt=arr.slice(0); 
-            delaycallback(k*100,arrt);
+            var arrt = arr.slice(0);
+            delaycallback(k * 10, arrt);
         }
-        t=arr[curr];
-        arr[curr]=arr[r];
-        arr[r]=t;
-        quicksort(arr,l,curr-1);
-        quicksort(arr,curr+1,r);
+        t = arr[curr];
+        arr[curr] = arr[r];
+        arr[r] = t;
+        quicksort(arr, l, curr - 1);
+        quicksort(arr, curr + 1, r);
     }
-    
+
 }
-// quicksort(arr,0, arr.length -1);
+generateRandomValued();
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+function callsorting(algo) {
+    arr = shuffle(arr);
+    if (algo == "bubble") {
+        bubblesort(arr,ctxofbubble);
+    } else if (algo == "quick") {
+        quicksort(arr, 0, arr.length - 1);
+    }
+}
+// var canvases=document.getElementsByTagName(canvas);
